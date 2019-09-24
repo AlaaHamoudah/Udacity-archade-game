089db1d10917fe2ba71db266458f8c59b0992aad
AlaaHamoudah <alaakhalil11@gmail.com>
2019-09-24 05:41:52 +0300

    Trying to move enemies and player

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
+const x_width = 101;
+const y_height = 83;
+
 // Enemies our player must avoid
 var Enemy = function (x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get starte
     this.x = x;
     this.y = y;
+    this.speed = 200;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -11,10 +15,15 @@ var Enemy = function (x, y) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function () {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    console.log((this.x +  x_width))
+     setInterval(function(){
+        ctx.drawImage(Resources.get(this.sprite), (this.x +  x_width), this.y);
+    }, dt * this.speed)
+
 
 };
 
@@ -39,19 +48,26 @@ class Player {
     update(params) {
 
     }
-    render() {
-        console.log(this.sprite)
+    render() { 
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
     handleInput(keyCode) {
+        
         switch (keyCode) {
-            case '37':  //left
+            case 'left':   
+                 console.log("left key"+ (this.x - x_width) + this.sprite)
+                  
+                 let width =  this.x - 101;
+                 //ctx.drawImage(Resources.get(this.sprite), 202 , this.y);
+                 console.log("done")
                 break;
-            case '38':  //up
+            case 'up':  //up
                 break;
-            case '39':  //right
+            case 'right':  //right
+                console.log("right")
+                ctx.drawImage(Resources.get(this.sprite), (this.x + x_width), this.y);
                 break;
-            case '40':  //down
+            case 'down':  //down
                 break;
 
         }
@@ -68,13 +84,13 @@ class Player {
 // Place the player object in a variable called player
 
 const allEnemies = [];
-allEnemies.push(new Enemy(101, 83));
-allEnemies.push(new Enemy(101*2,83*2));
-allEnemies.push(new Enemy(101*3, 83*3));
-allEnemies.push(new Enemy(101*4, 83*4));
-allEnemies.push(new Enemy(0, 83));
-allEnemies.push(new Enemy(101, 83*3));
-const player = new Player(101*3,  83*5);
+allEnemies.push(new Enemy(x_width, y_height));
+allEnemies.push(new Enemy(x_width*2,y_height*2));
+allEnemies.push(new Enemy(x_width*3, y_height*3));
+allEnemies.push(new Enemy(x_width*4, y_height*4));
+allEnemies.push(new Enemy(0, y_height));
+allEnemies.push(new Enemy(x_width, y_height*3));
+const player = new Player(x_width*3,  y_height*5);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -85,6 +101,5 @@ document.addEventListener('keyup', function (e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
